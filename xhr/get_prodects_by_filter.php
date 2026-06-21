6fd54e2af354e7e9d8f60409480932dc08bc8a1d
ocean22092004 <vthadaiduong@gmail.com>
2026-06-21 17:45:23 +0700

    1

--- xhr/get_prodects_by_filter.php
+++ xhr/get_prodects_by_filter.php
@@ -1,14 +1,16 @@
-<?php 
+<?php // Product filter endpoint with optional seller filtering. ?>
 if ($f == 'get_prodects_by_filter') {
     $data['status'] = 400;
     $cat_id = !empty($_POST['cat_id']) ? Wo_Secure($_POST['cat_id']) : '';
     $distance = !empty($_POST['distance']) ? Wo_Secure($_POST['distance']) : '';
     $price_sort = !empty($_POST['price_sort']) ? Wo_Secure($_POST['price_sort']) : '';
     $sub_id = !empty($_POST['sub_id']) ? Wo_Secure($_POST['sub_id']) : '';
+    $user_id = (!empty($_POST['user_id']) && is_numeric($_POST['user_id'])) ? Wo_Secure($_POST['user_id']) : 0;
     $products = Wo_GetProducts(array('c_id' => $cat_id,
                                      'length' => $distance,
                                      'order_by' => $price_sort,
-                                     'sub_id' => $sub_id));
+                                     'sub_id' => $sub_id,
+                                     'user_id' => $user_id));
     $html = '';
     if (!empty($products)){
         foreach ($products as $key => $wo['product']) {
