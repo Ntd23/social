40346bde710422f6cbe1fff05c552a6f37c0b866
nguyen doan hoang <nguyendoanhoang4@gmail.com>
2026-06-23 17:02:02 +0700

    fix

--- xhr/address.php
+++ xhr/address.php
@@ -1,7 +1,7 @@
 <?php
 if ($f == "address") {
 	if ($s == 'add') {
-		if (!empty($_POST['name']) && !empty($_POST['phone']) && !empty($_POST['country']) && !empty($_POST['city']) && !empty($_POST['zip']) && !empty($_POST['address'])) {
+		if (!empty($_POST['name']) && !empty($_POST['phone']) && !empty($_POST['country']) && !empty($_POST['city']) && !empty($_POST['address'])) {
 			$id = $db->insert(T_USER_ADDRESS,array('name' => Wo_Secure($_POST['name']),
 		                                'phone' => Wo_Secure($_POST['phone']),
 		                                'city' => Wo_Secure($_POST['city']),
@@ -39,7 +39,7 @@ if ($f == "address") {
 		}
 	}
 	if ($s == 'edit') {
-		if (!empty($_POST['name']) && !empty($_POST['phone']) && !empty($_POST['country']) && !empty($_POST['city']) && !empty($_POST['zip']) && !empty($_POST['address']) && !empty($_POST['id']) && is_numeric($_POST['id']) && $_POST['id'] > 0) {
+		if (!empty($_POST['name']) && !empty($_POST['phone']) && !empty($_POST['country']) && !empty($_POST['city']) && !empty($_POST['address']) && !empty($_POST['id']) && is_numeric($_POST['id']) && $_POST['id'] > 0) {
 			$address = $db->where('id',Wo_Secure($_POST['id']))->getOne(T_USER_ADDRESS);
 			if (!empty($address) && ($address->user_id == $wo['user']['user_id'] || IsAdmin())) {
 				$db->where('id',$address->id)->update(T_USER_ADDRESS,array('name' => Wo_Secure($_POST['name']),
